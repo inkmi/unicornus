d9cdc76e97f8d77627c5739a8c43107870819ae9
Stephan Schmidt <stephan.schmidt@gmail.com>
2024-02-27 10:12:39 +0100

    Change package from pkg to uni

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -85,12 +85,14 @@ func wrap(fileName string, f func(http.ResponseWriter, *http.Request)) func(http
 		content := highlight(removeSpecialLines(string(data)))
 		fmt.Fprintf(w, "<!DOCTYPE html><html><body>")
 		fmt.Fprintf(w, "<a href=\"/\"> <-- Back</a>")
+		fmt.Fprintf(w, "<div style=\"display: flex;\"><div>")
 		fmt.Fprintf(w, "<h2>Example Form</h2>")
-
 		f(w, req)
-
-		fmt.Fprintf(w, "<h2 style=\"margin-top: 4rem;\">%s</h2>\n", fileName)
-		fmt.Fprintf(w, "<pre style=\"background:#282a36;;padding: 1rem;\">\n"+string(content)+"\n</pre>")
+		fmt.Fprintf(w, "</div><div style=\"width:5rem;\">")
+		fmt.Fprintf(w, "</div><div>")
+		fmt.Fprintf(w, "<h2 style=\"\">%s</h2>\n", fileName)
+		fmt.Fprintf(w, "<div style=\"background:#282a36;padding: 1rem;\">\n"+string(content)+"\n</div>")
+		fmt.Fprintf(w, "</div>")
 		fmt.Fprintf(w, "</body></html>")
 	}
 }
